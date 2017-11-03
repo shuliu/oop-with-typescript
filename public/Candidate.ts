1af6d97e619df4c5f5e1e94869464ab7c66ae883
yunshian <yunshian.shiao@senao.com.tw>
2017-11-03 20:28:24 +0800

    add Candidate

---
+++ public/Candidate.ts
@@ -0,0 +1,26 @@
+
+export class Candidate {
+  private config;
+  private fileDateTime;
+  private name;
+  private processName;
+  private size;
+
+  constructor() {}
+
+  get FileDateTime() {
+    return;
+  }
+
+  get Name() {
+    return;
+  }
+
+  get ProcessName() {
+    return;
+  }
+
+  get Size() {
+    return;
+  }
+}
